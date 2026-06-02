0cd2d0a3511c3f93971b06b383d60ccb7f57b61d
meehai <mihaicristianpirvu@gmail.com>
2026-06-02 09:29:44 +0300

    move clock to utils

---
+++ ecs/utils.py
@@ -0,0 +1,27 @@
+"""clock.py - raylib-based clock allowing to 'drain' it such as to keep a steady FPS rate and consitent physics"""
+import raylib as rl
+
+class Clock:
+    """clock used for physics with fixed DT in main loops"""
+
+    def __init__(self, dt: float, max_ticks: int):
+        self.dt = dt
+        self.max_ticks = max_ticks
+        self.prev_time = rl.GetTime()
+        self.accumulator = 0
+
+    def tick(self):
+        """tick once by adding the delta between prev frame and now"""
+        now = rl.GetTime()
+        frame_time = now - self.prev_time
+        self.prev_time = now
+        self.accumulator += frame_time
+
+    def drain(self):
+        """drain the accumulator. in main loop: for _ in clock.drain(): ..."""
+        n_ticks = 0
+        while self.accumulator >= self.dt and n_ticks < self.max_ticks:
+            yield
+            self.accumulator -= self.dt
+            n_ticks += 1
+        self.accumulator = min(self.accumulator, self.dt) # Drop residual debt instead of it piling up across frames
--- main.py
+++ main.py
@@ -8,37 +8,11 @@ import raylib as rl
 from loggez import loggez_logger as logger
 
 from ecs import World
+from ecs.utils import Clock
 
 Point2D = tuple[float, float]
 DT = 1 / 100
 MAX_SUBTICKS_PER_RENDER_TICK = 3
-
-# utils
-
-class Clock:
-    """clock used for physics with fixed DT in main loops"""
-
-    def __init__(self, dt: float, max_ticks: int):
-        self.dt = dt
-        self.max_ticks = max_ticks
-        self.prev_time = rl.GetTime()
-        self.accumulator = 0
-
-    def tick(self):
-        """tick once by adding the delta between prev frame and now"""
-        now = rl.GetTime()
-        frame_time = now - self.prev_time
-        self.prev_time = now
-        self.accumulator += frame_time
-
-    def drain(self):
-        """drain the accumulator. in main loop: for _ in clock.drain(): ..."""
-        n_ticks = 0
-        while self.accumulator >= self.dt and n_ticks < self.max_ticks:
-            yield
-            self.accumulator -= self.dt
-            n_ticks += 1
-        self.accumulator = min(self.accumulator, self.dt) # Drop residual debt instead of it piling up across frames
 
 # traits
 
