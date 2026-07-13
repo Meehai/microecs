03eaea314033bd0d007f64aff42aafb8d01fb662
meehai <mihaicristianpirvu@gmail.com>
2026-07-13 19:34:00 +0300

    setuppy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 NAME = "microecs"
-VERSION = "0.6.0"
+VERSION = "0.6.1"
 DESCRIPTION = "MicroECS: Minimal Entity Component System (ECS) in python and numpy"
 URL = "https://gitlab.com/meehai/microecs"
 
